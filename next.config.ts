2a2604716ac64049a3ab96449553857e02fcdbb3
Ertke Kec <ertkekec@gmail.com>
2026-02-23 23:56:52 +0300

    Security: Fixed Permissions-Policy blocking camera, mic and location

--- next.config.ts
+++ next.config.ts
@@ -53,7 +53,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: 'Permissions-Policy',
-            value: 'camera=(), microphone=(), geolocation=()'
+            value: 'camera=(self), microphone=(self), geolocation=(self)'
           },
           {
             key: 'X-XSS-Protection',
